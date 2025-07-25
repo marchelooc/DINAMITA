cbca0b8a2296db27dfe84eed8e0e923e96e6b959
Fernano25 <mollofernando374@gmail.com>
2025-07-25 15:46:09 -0400

    "Test Case 001 sobre obtener lista de Departamentos - Fernando Mollo"

---
+++ Test Case 001/TestCase.py
@@ -0,0 +1,27 @@
+# ID: 001
+# Titulo: Obtener los departamentos
+#
+# Descripción: El usuario debe obtener todos los datos de los departamentos
+# Precondiciones:
+# Deben existir departamentos creados
+# '
+# Ambiente: Acceso al metmuseum.com
+# Usuario con el que tiene acceso
+#
+# Pasos
+# 1 Acceder al Login
+# 2 Abrir Postman
+# 3 Copiar el URL
+# 4 Seleccionar el método GET
+# 5 Click en el botón Send
+# 6 Comparar la salida con
+#
+# Resultados Esperado: código de respuesta 200
+#
+# Evidencias
+# Foto de Postman
+# CURL
+#
+# Prioridad: Media
+# Postcondicon: Eliminar el objeto creado en la precondición
+# Categoria: Smoke
