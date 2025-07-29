8edfe5880423c917cacb63ea9255640e8b8c4580
Sergio <201801656@est.umss.edu>
2025-07-29 19:58:12 -0400

    Shemma

---
+++ TC003.py
@@ -0,0 +1,33 @@
+import requests
+import pytest
+import jsonschema
+
+@pytest.mark.smoke
+def testObtenerListaDeTutoresActivosCorrectamente():
+    url = "https://backend.clubinfinitychess.com/"
+    endpoint = "agregarCurso"
+    lista_url = url + endpoint
+    payload = {
+        "CODCURSO": "",
+        "CURSO": ""
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    response = requests.post(lista_url, json=payload, headers=headers)
+    assert response.status_code == 200
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "CODCURSO": {"type": "string"},
+            "CURSO": {"type": "string"},
+            "ESTADO": {"type": "string"}
+        },
+        "required": ["CODCURSO", "CURSO", "ESTADO"]
+    }
+    try:
+        jsonschema.validate(instance=response.json(), schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        pytest.fail(f"JSON schema no coincide: {err}")
