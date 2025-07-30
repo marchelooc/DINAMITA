0abbdec9bfe49982f89e57557851e43f2f1d7004
Fernano25 <mollofernando374@gmail.com>
2025-07-30 18:49:32 -0400

    Tarea Post Schema Validation

---
+++ Test Case 003/Schema.py
@@ -0,0 +1,34 @@
+import requests 
+import pytest
+import jsonschema # type: ignore
+
+
+@pytest.mark.smoke
+def testObtenerListaDeTutoresActivosCorrectamente():
+    url = "https://backend.clubinfinitychess.com/"
+    endpoint = "agregarCurso"
+    lista_url = url + endpoint
+    payload = {
+        "CODCURSO": "2027Curso",
+        "CURSO": "Curso"
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    response = requests.post(lista_url, json=payload, headers=headers)
+    assert response.status_code == 201
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "CODCURSO": {"type": "string"},
+            "CURSO": {"type": "string"},
+            "ESTADO": {"type": "string"}
+        },
+        "required": ["CODCURSO", "CURSO", "ESTADO"]
+    }
+    try:
+        jsonschema.validate(instance=response.json(), schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        pytest.fail(f"JSON schema no coincide: {err}")
